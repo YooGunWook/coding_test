4fadd540d640a701ac7cde2193c9da2e723a16e9
YooGunWook <gunwook0307@yonsei.ac.kr>
2020-04-26 21:00:42 +0900

    코딩테스트

--- 카카오/n진수 게임.py
+++ 카카오/n진수 게임.py
@@ -32,30 +32,29 @@ def solution(n, t, m, p):
             answer += i[j]
 
             
-    while turn < t:
-        for i in range(0,len(answer)):
-            # m과 p가 같은 경우가 있기 때문에 if문을 활용한다. 
-            # turn에 1을 더해주고 count_player는 초기화
-            if count_player == m:
-                if count_player == p:
-                    real_answer += answer[i]
-                    count_player = 1
-                    turn += 1
-                else:
-                    count_player = 1
-                    turn += 1
-            # m과 p가 같지 않은 경우
-            # count_player를 1씩 더해주고, p와 같아지면 real_answer에 넣어준다. 
+    for i in range(0,len(answer)):
+        # m과 p가 같은 경우가 있기 때문에 if문을 활용한다. 
+        # turn에 1을 더해주고 count_player는 초기화
+        if count_player == m:
+            if count_player == p:
+                real_answer += answer[i]
+                count_player = 1
+                turn += 1
             else:
-                if count_player == p:
-                    real_answer += answer[i]
-                    count_player += 1
-                else:
-                    count_player += 1
+                count_player = 1
+                turn += 1
+        # m과 p가 같지 않은 경우
+        # count_player를 1씩 더해주고, p와 같아지면 real_answer에 넣어준다. 
+        else:
+            if count_player == p:
+                real_answer += answer[i]
+                count_player += 1
+            else:
+                count_player += 1
             
-            # while문이 잘 안먹혀서 씀. turn이 t와 같아지면 break
-            if turn == t:
-                break
+        # while문이 잘 안먹혀서 씀. turn이 t와 같아지면 break
+        if turn == t:
+            break
             
     
     return real_answer
