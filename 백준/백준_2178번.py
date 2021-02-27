adcc78e36e615f91ed5a81fb26541f0835ab9cea
YooGunWook <gunwook0307@yonsei.ac.kr>
2021-02-27 17:25:16 +0900

    add coding test code

--- 백준/백준_2178번.py
+++ 백준/백준_2178번.py
@@ -5,7 +5,7 @@ miro = []
 for _ in range(n):
     miro.append(list(map(int, input())))
 
-dx, dy = [-1, 1, 0, 0], [0,0, -1, 1]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
 visited = [[0] * m for _ in range(n)]
 queue = collections.deque([(0,0)])
@@ -26,3 +26,52 @@ while queue:
             if visited[nx][ny] == 0 and miro[nx][ny] == 1:
                 visited[nx][ny] = visited[x][y] + 1
                 queue.append((nx, ny))
+                       
+                
+                
+                
+                
+                
+n, m = list(map(int, input().split(' ')))
+miro = []
+for _ in range(n):
+    miro.append(list(map(int, input())))
+
+visited = [[0] * m for _ in range(n)]
+queue = collections.deque([[0,0]])
+visited[0][0] = 1
+
+dx, dy = [-1,1,0,0], [0,0,-1,1]
+
+while queue:
+    x,y = queue.popleft()
+    
+    if x == n - 1 and y == m - 1:
+        print(visited[x][y])
+        break
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if visited[nx][ny] == 0 and miro[x][y] == 1:
+            visited[nx][ny] = visited[x][y] + 1
+            queue.append([nx,ny])
+        
+                
+
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+        
